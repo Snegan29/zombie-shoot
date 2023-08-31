27424599e54c552990e71d8087d3937ba3378bf2
Snegan29 <pssj2914@gmail.com>
2023-08-31 09:22:14 +0530

    Made some changes

--- game.js
+++ game.js
@@ -1,23 +1,16 @@
 // Iteration 1: Declare variables required for this game
-
 // Iteration 1.2: Add shotgun sound
-
 // Iteration 1.3: Add background sound
-
 // Iteration 1.4: Add lives
-
 // Iteration 2: Write a function to make a zombie
-
 // Iteration 3: Write a function to check if the player missed a zombie
-
 // Iteration 4: Write a function to destroy a zombie when it is shot or missed
-
 // Iteration 5: Creating timer
-
 // Iteration 6: Write a code to start the game by calling the first zombie
-
 // Iteration 7: Write the helper function to get random integer
 
+
+//shotgun sound when clicked.
 const game_body = document.getElementById("game-body")
 
 let shotgunbgm = new Audio("./assets/shotgun.wav")
@@ -26,12 +19,13 @@ game_body.onclick = () => {
     shotgunbgm.play()
 }
 
+//background music
 let bgmusic = new Audio("./assets/bgm.mp3")
 bgmusic.play()
 bgmusic.loop = true 
 
-//random numbers.
 
+//to generate new zombies.
  zombieID = 0
 function generateZombie(){
     let num = generateUniqueNums(1,7)
@@ -63,13 +57,16 @@ function generateUniqueNums(min,max){
     return Math.floor(Math.random()*(max-min))+min
 }
 
-// check if zombies missed 
-let lives = 2
-let time = 20
+// check if zombies missed and if we are losing lives.
+let lives = 4
+let time = 60
+const widthOfLine = document.getElementById("max-lives")
 
 function missedZombies(zombie){
     if(zombie.getBoundingClientRect().top<=0){
         lives--
+        const maxlives = document.getElementById("max-lives");
+        maxlives.style.width = lives*25+"%";
         if(lives == 0){
             location.href="./game-over.html"
         }
@@ -79,8 +76,8 @@ function missedZombies(zombie){
 missedZombies(zombie)
 
 
-// timer funtion
 
+// timer funtion
 setInterval(timer,1000)
 
 function timer(){
@@ -92,3 +89,5 @@ function timer(){
         missedZombies(zombie)
     }
 }
+
+
